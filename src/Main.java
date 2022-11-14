6a08ec9f121a3ccd28f111c142938c0f869bef37
itsOtto <luhatuman@yandex.ru>
2022-11-14 23:52:22 +0900

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,19 @@ public class Main {
         if (userAge < 18) {
             System.out.println("Возраст совершеннолетия еще не наступил, нужно немного подождать");
         }
+
+        //Task 2
+        System.out.println();
+        System.out.println("Task 2");
+        byte person = 18; //Вводим возраст человека
+        if (person >= 7 && person < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (person >= 18 && person < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (person >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
     }
 }
