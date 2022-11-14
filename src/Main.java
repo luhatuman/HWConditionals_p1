b8340c7b46a4057e21bdf89fd9f4f9b018c73942
itsOtto <luhatuman@yandex.ru>
2022-11-14 23:30:15 +0900

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task 1
+        System.out.println("Task 1");
+        byte userAge = 17;
+        if (userAge >= 18) {
+            System.out.println("Поздравляю с совершеннолетием!");
+        }
+        if (userAge < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно немного подождать");
+        }
+    }
+}
